4e3bfeb35bb400fe42d20617294be8e432948606
serhii <kovalenkoserg1990@gmail.com>
2019-10-17 00:38:29 +0300

    done template refactoring

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,25 +1,4 @@
 
 
-class App {
-  constructor() {
-  }
-  itemHeight() {
-    let item = document.querySelectorAll('.news-item__title'),
-        initHeight = 0;
-    for(let i = 0; i < item.length; i++) {
-      if(item[i].clientHeight > initHeight) {
-        initHeight = item[i].clientHeight;
-      }
-      item[i].style.height = initHeight + 'px';
-    }
-  };
-  init () {
-    this.itemHeight();
-  }
-}
-
-let app = new  App();
-app.init();
-
 
 
